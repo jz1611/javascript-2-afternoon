3b89fbfe7d75a745d8fc3c72ae421eb4bb52f480
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-05 18:59:28 -0700

    Completed problem 4

--- practice.js
+++ practice.js
@@ -51,7 +51,7 @@ favoriteThings['brand'] = 'REI';
 */
 
 favoriteThings.food = 'Chicken Nuggets';
-faoriteThings['book'] = 'Harry Potter';
+favoriteThings['book'] = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -63,13 +63,16 @@ faoriteThings['book'] = 'Harry Potter';
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+var backPack = {};
+var item = 'firstPocket';
+backPack[item] = 'chapstick';
+backPack.color = 'green';
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -77,7 +80,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
